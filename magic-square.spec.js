17eb5c96155ccce7900177dd0bbd75b8495c56ea
Fabio Elizandro <fabio@elizandro.com.br>
2019-06-30 13:47:38 +1000

    setup

---
+++ magic-square.spec.js
@@ -0,0 +1,15 @@
+function formingMagicSquare(s) {
+  return 1
+}
+
+describe('formingMagicSquare', () => {
+  it('foo', () => {
+    const result = formingMagicSquare([
+      [],
+      [],
+      [],
+    ])
+
+    expect(result).toEqual(1)
+  })
+})
